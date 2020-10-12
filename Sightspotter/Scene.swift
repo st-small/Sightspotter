54ebcfbc630c98e0484743fbe52ba4e9f616a123
Станислав Шияновский <st-small@rambler.ru>
2020-10-12 19:30:05 +0300

    Completed project.

--- Sightspotter/Scene.swift
+++ Sightspotter/Scene.swift
@@ -10,8 +10,54 @@ import ARKit
 
 class Scene: SKScene {
     
+    let remainingLabel = SKLabelNode()
+    var timer: Timer!
+
+    var targetsCreated = 0
+    var targetCount = 0 {
+        didSet {
+            remainingLabel.text = "Remaining: \(targetCount)"
+        }
+    }
+
+    let startTime = Date()
+    
     override func didMove(to view: SKView) {
-        // Setup your scene here
+        remainingLabel.fontSize = 36
+        remainingLabel.fontName = "AmericanTypewriter"
+        remainingLabel.color = .white
+        remainingLabel.position = CGPoint(x: 0, y: view.frame.midY - 50)
+        addChild(remainingLabel)
+        targetCount = 0
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+            self.createTarget()
+        }
+    }
+    
+    private func createTarget() {
+        if targetsCreated == 20 {
+            timer?.invalidate()
+            timer = nil
+            return
+        }
+
+        targetsCreated += 1
+        targetCount += 1
+
+        guard let sceneView = self.view as? ARSKView else { return }
+
+        let xRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0...1), 1, 0, 0))
+        let yRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0...1), 0, 1, 0))
+        let rotation = simd_mul(xRotation, yRotation)
+
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.5
+        let transform = simd_mul(rotation, translation)
+
+        // Add a new anchor to the session
+        let anchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: anchor)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -19,21 +65,38 @@ class Scene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let sceneView = self.view as? ARSKView else {
-            return
-        }
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
         
-        // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
+        let hit = nodes(at: location)
+        
+        if let sprite = hit.first {
+            let scaleOut = SKAction.scale(to: 2, duration: 0.2)
+            let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+            let group = SKAction.group([scaleOut, fadeOut])
+            let sequence = SKAction.sequence([group, SKAction.removeFromParent()])
+            sprite.run(sequence)
             
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
-            let transform = simd_mul(currentFrame.camera.transform, translation)
+            targetCount -= 1
             
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            sceneView.session.add(anchor: anchor)
+            if targetsCreated >= 20 && targetCount == 0 {
+                gameOver()
+            }
         }
+    }
+    
+    private func gameOver() {
+        remainingLabel.removeFromParent()
+
+        let gameOver = SKSpriteNode(imageNamed: "gameOver")
+        addChild(gameOver)
+
+        let timeTaken = Date().timeIntervalSince(startTime)
+        let timeLabel = SKLabelNode(text: "Time taken: \(Int(timeTaken)) seconds")
+        timeLabel.fontSize = 36
+        timeLabel.fontName = "AmericanTypewriter"
+        timeLabel.color = .white
+        timeLabel.position = CGPoint(x: 0, y: -view!.frame.midY + 50)
+        addChild(timeLabel)
     }
 }
